02ea194dbe77efe11fb0c8006fee7cd4ddda5825
nmargulies <nmargulies@college.harvard.edu>
2018-07-10 13:27:44 -0700

    Camera Working

---
+++ app/src/main/java/me/nmargulies/parsetegram/EditActivity.java
@@ -0,0 +1,88 @@
+package me.nmargulies.parsetegram;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+
+import me.nmargulies.parsetegram.model.Post;
+
+public class EditActivity extends AppCompatActivity {
+    String filepath;
+    EditText descriptionInput;
+    Bitmap takenImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        descriptionInput = findViewById(R.id.etCaption);
+        filepath = getIntent().getStringExtra("filepath");
+
+        // by this point we have the camera photo on disk
+        takenImage = BitmapFactory.decodeFile(filepath);
+
+        // RESIZE BITMAP, see section below
+        // Load the taken image into a preview
+        ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+        ivPreview.setImageBitmap(takenImage);
+    }
+
+    public void onPost(View view) {
+
+        final String description = descriptionInput.getText().toString();
+        final ParseUser user = ParseUser.getCurrentUser();
+        final File file = new File(filepath);
+        final ParseFile parseFile = new ParseFile(file);
+
+        parseFile.saveInBackground(new SaveCallback() {
+
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("HomeActivity", "Save file success.");
+                    createPost(description, parseFile, user);
+                    finish();
+
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
+
+    }
+
+    private void createPost(String description, ParseFile imageFile, ParseUser user) {
+        final Post newPost = new Post();
+        newPost.setDescription(description);
+        newPost.setImage(imageFile);
+        newPost.setUser(user);
+
+        newPost.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("HomeActivity", "Create post success!");
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/me/nmargulies/parsetegram/HomeActivity.java
+++ app/src/main/java/me/nmargulies/parsetegram/HomeActivity.java
@@ -1,12 +1,20 @@
 package me.nmargulies.parsetegram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -22,48 +30,29 @@ import me.nmargulies.parsetegram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private static final String imagePath = "/sdcard/DCIM/Camera/IMG_20180709_160522.jpg";
-    private EditText descriptionInput;
-    private Button createButton;
     private Button refreshButton;
     private Button logoutButton;
-    private Button cameraButton;
+
+    // how to launch the camera
+    public final String APP_TAG = "MyCustomApp";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        //getSupportActionBar().setLogo(R.mipmap.ic_launcher_twitter_round);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
-            descriptionInput = findViewById(R.id.description_et);
-            createButton = findViewById(R.id.create_btn);
             refreshButton = findViewById(R.id.refresh_btn);
             logoutButton = findViewById(R.id.logoutbtn);
-
-            createButton.setOnClickListener(new View.OnClickListener(){
-                @Override
-                public void onClick(View view) {
-                    final String description = descriptionInput.getText().toString();
-                    final ParseUser user = ParseUser.getCurrentUser();
-
-                    //TODO -- take photo from camera and pass into it
-                    final File file = new File(imagePath);
-                    final ParseFile parseFile = new ParseFile(file);
-
-                    parseFile.saveInBackground(new SaveCallback() {
-                        @Override
-                        public void done(ParseException e) {
-                            if (e == null) {
-                                Log.d("HomeActivity", "Save file success.");
-                                createPost(description, parseFile, user);
-                            } else {
-                                e.printStackTrace();
-                            }
-                        }
-                    });
-                }
-            });
 
             refreshButton.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -81,7 +70,6 @@ public class HomeActivity extends AppCompatActivity {
                 }
             });
 
-
         } else {
             // return to the signin activity
             final Intent intent = new Intent(HomeActivity.this, MainActivity.class);
@@ -90,23 +78,6 @@ public class HomeActivity extends AppCompatActivity {
         }
     }
 
-    private void createPost(String description, ParseFile imageFile, ParseUser user) {
-        final Post newPost = new Post();
-        newPost.setDescription(description);
-        newPost.setImage(imageFile);
-        newPost.setUser(user);
-
-        newPost.saveInBackground(new SaveCallback() {
-            @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Log.d("HomeActivity", "Create post success!");
-                } else {
-                    e.printStackTrace();
-                }
-            }
-        });
-    }
 
     private void loadTopPosts() {
         final Post.Query postQuery = new Post.Query();
@@ -128,4 +99,58 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+
+                Intent i = new Intent(HomeActivity.this, EditActivity.class);
+                i.putExtra("filepath", photoFile.getAbsolutePath());
+                startActivity(i);
+
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
 }
